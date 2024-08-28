b184efd42eacd00d3d52c3f384e8c7f8e218dadf
Kavik22 <dima@lotsman.org>
2024-08-28 17:06:33 +0500

    first commit

---
+++ resources/js/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hehe</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default {
+  components: {},
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
